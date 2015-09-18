b042366a08d000e7d45eecc4ab49a19c478955f5
nabijaczleweli-vm <nabijaczleweli@gmail.com>
2015-09-18 23:15:47 +0100

    Upload correct filenames

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 uploader="./dropbox_uploader.sh -f .dropbox_uploader"
 subfolder_name="$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE-$TRAVIS_BRANCH"
-outfilename="platformake-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
+defaultoutfilename="platformake-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
+newestoutfilename="platformake-$COMPILER_NAME"
 
 
 curl -Os "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh"
@@ -12,6 +13,12 @@ chmod +x dropbox_uploader.sh
 for subfolder in "$subfolder_name" "0-newest"
 do
 	$uploader mkdir "$subfolder"
+	if [ "$subfolder" == "0-newest" ]
+	then
+		outfilename="$newestoutfilename"
+	else
+		outfilename="$defaultoutfilename"
+	fi
 	$uploader upload out/platformake.out "$subfolder/$outfilename"
 	for file in README.md LICENSE
 	do
